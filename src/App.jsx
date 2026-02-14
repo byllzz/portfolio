c207b539f1664460f4ee884ff62ae7ab0e42bef8
Bilal Malik <bilalmlkdev@gmail.com>
2026-02-14 16:52:42 +0500

    adding font-faces to App.css

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 
 export default function App() {
   return (
-    <div>App</div>
+    <div style={{
+      fontFamily: "Instrument Serif" , fontSize : "48px" , fontWeight : 600
+    }}>portfolio App</div>
   )
 }
