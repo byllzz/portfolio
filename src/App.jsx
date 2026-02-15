f33454119f678b97168b167b5239c3b335edc5be
Bilal Malik <bilalmlkdev@gmail.com>
2026-02-15 20:58:12 +0500

    about section added

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,12 @@ import Footer from './sections/Footer';
 import Contact from './sections/Contact';
 import Background from './sections/Background';
 import Projects from './sections/Projects';
+import About from './sections/About';
 
 export default function App() {
   return (
   <>
+  <About />
   <Projects />
   <SkillsSection />
    <Background />
