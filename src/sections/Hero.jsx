2a0d15f269979c75e746db9086362b79b6146db1
Bilal Malik <bilalmlkdev@gmail.com>
2026-02-16 17:59:21 +0500

    Hero section updated

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -1,17 +1,21 @@
 import React from 'react';
 import { motion } from 'framer-motion';
-import styles from '../styles/Hero.module.css'
+import styles from '../styles/Hero.module.css';
+
+import flower from '../assets/hero-banners/flowers.webp';
+import flower1 from '../assets/hero-banners/flowers1.webp';
 
 const letters = ['B','i','l','a','l','m','a','l','i','k'];
+const banners = [flower, flower1];
 
 export default function Test() {
   return (
     <div className={styles.heroSection}>
       <div className={styles.heroBanners}>
         {letters.map((letter, index) => {
-          // Random delay for pulse animation
           const pulseDelay = Math.random() * 2;
           const motionDelay = Math.random() * 0.3;
+          const bannerImage = banners[index % banners.length];
 
           return (
             <motion.div
@@ -19,9 +23,14 @@ export default function Test() {
               key={index}
               initial={{ y: -200, opacity: 0 }}
               animate={{ y: 0, opacity: 1 }}
-              transition={{ duration: 0.4, delay: motionDelay }} // fast
+              transition={{ duration: 0.4, delay: motionDelay }}
             >
-              hello
+              <img
+                src={bannerImage}
+                alt="banner"
+                className={styles.bannerImg}
+              />
+
               <div className={styles.overlap}>
                 <div
                   className={styles.overlapTxt}
@@ -31,16 +40,15 @@ export default function Test() {
                 </div>
               </div>
             </motion.div>
-
-
           );
         })}
       </div>
-            <div className={styles.heroLights}>
-                    <span>Web De_(s/v),</span>
-                    <span>Graphic Designer,</span>
-                    <span>AI/ML Solutions Architect</span>
-                  </div>
+
+      <div className={styles.heroLights}>
+        <span>Web De_(s/v),</span>
+        <span>Graphic Designer,</span>
+        <span>AI/ML Solutions Architect</span>
+      </div>
     </div>
   );
 }
